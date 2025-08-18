16df64bc982d48719387fd9132931dccdfe32e1b
Ri9mochi <rikujobs211111@gmail.com>
2025-08-18 23:07:38 +0900

    shipping_informationsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_18_084119) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_18_094911) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_18_084119) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "shipping_informations", charset: "utf8mb3", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shipping_informations_on_order_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -86,4 +99,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_18_084119) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "shipping_informations", "orders"
 end
